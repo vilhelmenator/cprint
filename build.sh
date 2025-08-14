59e9e0e5bcfce3a1cc6b7fde6ad16a8de3eaaf28
Vilhelm Páll Sævarsson <vilhelmenator@gmail.com>
2025-08-14 17:41:05 +0000

    simple build

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
-clang -O3 -march=native -flto=thin -fomit-frame-pointer -fno-exceptions -fno-rtti -fvisibility=hidden   -mno-red-zone -ftls-model=initial-exec -Wl,-O3 -Wl  -DNDEBUG -g0  -o test  *.c 
+#!/bin/bash
+
+if [ "$1" == "debug" ]; then
+    echo "Building debug version..."
+    clang -O0 -g -Wall -o test *.c
+else
+    echo "Building release version..."
+    clang -O3 -march=native -flto=thin -fomit-frame-pointer -fno-exceptions -fno-rtti -fvisibility=hidden -mno-red-zone -ftls-model=initial-exec -Wl,-O3 -Wl -DNDEBUG -g0 -o test *.c
+fi
